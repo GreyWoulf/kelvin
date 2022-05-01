070aee919a0484c51adc8031b571d2d2efb963ac
GreyWoulf <93624560+GreyWoulf@users.noreply.github.com>
2022-05-01 16:54:26 -0400

    made a temperature function and made a celsius and fahrenheit conversions

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let kelvin = 293;
-let celsius = kelvin - 273;
-let fahrenheit = Math.floor((celsius * (9/5) + 32));
+function temperature (unitTemp, temp, toTemp){
+    let kelvin = 0;
+    let celsius = 0;
+    let fahrenheit = 0;
+    if(unitTemp.toLowerCase() === 'kelvin' && toTemp.toLowerCase() === 'celsius'){
+        kelvin += temp;
+        celsius = kelvin - 273;
+        return `The temperature is ${celsius} degrees in Celsius`
+    }
+    else if (unitTemp.toLowerCase() === 'kelvin' && toTemp.toLowerCase() === 'farenheit'){
+        kelvin += temp;
+        celsius = kelvin - 273;
+        fahrenheit = Math.floor(celsius * (9/5) + 32);
+        return `The temperature is ${fahrenheit} degrees in Fahrenheit`
+    }
 
+}
+
+
+
+console.log(temperature('kelvin', 500, 'celsius'))
 
